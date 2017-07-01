16717b5f1f3e29dd86ed5323e8da129321072c1a
Shawn Krisman <krisman.shawn@gmail.com>
2017-07-01 16:39:39 -0700

    changed name of test.

--- src/__tests__/smashgg-parser.test.js
+++ src/__tests__/smashgg-parser.test.js
@@ -1,7 +1,9 @@
 import { normalizeUrl } from '../smashgg-parser';
 
-test('normalizeUrl', () => {
-  const originalUrl = 'https://smash.gg/tournament/norcal-smash-4-s-arcadian-runback/events';
-  const normalizedUrl = 'https://smash.gg/tournament/norcal-smash-4-s-arcadian-runback';
-  expect(normalizeUrl(originalUrl)).toEqual(normalizedUrl);
+describe('normalizeUrl', () => {
+  test('suffixes are removed', () => {
+    const originalUrl = 'https://smash.gg/tournament/norcal-smash-4-s-arcadian-runback/events';
+    const normalizedUrl = 'https://smash.gg/tournament/norcal-smash-4-s-arcadian-runback';
+    expect(normalizeUrl(originalUrl)).toEqual(normalizedUrl);
+  });
 });
