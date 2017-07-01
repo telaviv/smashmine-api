7a532910fbd9436a5bc1406cec4bee916e3f3105
Shawn Krisman <krisman.shawn@gmail.com>
2017-07-01 14:23:07 -0700

    wrote our first test.

---
+++ test/smashgg-parser.test.js
@@ -0,0 +1,7 @@
+import { normalizeUrl } from '../lib/smashgg-parser'
+
+test('normalizeUrl', () => {
+  const originalUrl = 'https://smash.gg/tournament/norcal-smash-4-s-arcadian-runback/events'
+  const normalizedUrl = 'https://smash.gg/tournament/norcal-smash-4-s-arcadian-runback'
+  expect(normalizeUrl(originalUrl)).toEqual(normalizedUrl)
+});
