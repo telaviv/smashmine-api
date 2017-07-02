d0a5982f447c7e3b12ff93632e14f94533fc920e
Shawn Krisman <krisman.shawn@gmail.com>
2017-07-01 23:25:52 -0700

    wrote isMatchingUrl

--- src/__tests__/smashgg-parser.test.js
+++ src/__tests__/smashgg-parser.test.js
@@ -1,4 +1,4 @@
-import { normalizeUrl } from '../smashgg-parser';
+import { normalizeUrl, isMatchingUrl } from '../smashgg-parser';
 
 describe('normalizeUrl', () => {
   it('removes suffixes', () => {
@@ -17,5 +17,17 @@ describe('normalizeUrl', () => {
     const originalUrl = 'http://smash.gg/tournament/cats/';
     const normalizedUrl = 'https://smash.gg/tournament/cats';
     expect(normalizeUrl(originalUrl)).toEqual(normalizedUrl);
+  });
+});
+
+describe('isMatchingUrl', () => {
+  it('matches smash.gg urls', () => {
+    const url = 'https://smash.gg/tournament/norcal-smash-4-s-arcadian-runback/events';
+    expect(isMatchingUrl(url)).toBeTruthy();
+  });
+
+  it('does not match challonge urls', () => {
+    const url = 'http://challonge.com/capcom_cup_2015';
+    expect(isMatchingUrl(url)).toBeFalsy();
   });
 });
--- src/smashgg-parser.js
+++ src/smashgg-parser.js
@@ -2,3 +2,7 @@ export function normalizeUrl(url) {
   const slug = url.match('.*smash.gg/tournament/([^/]+).*')[1];
   return `https://smash.gg/tournament/${slug}`;
 }
+
+export function isMatchingUrl(url) {
+  return url.match(/.*smash.gg.*/);
+}
